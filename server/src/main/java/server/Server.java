fbd17a208c86ef2e3b650ef8734ff479561eeece
edenevans111 <edenevans111@gmail.com>
2025-07-14 21:38:29 -0700

    I have maybe added endpoints correctly, there is a lot of red, but I think so far it's because I haven't made functions for everything yet

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -10,6 +10,12 @@ public class Server {
         Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
+        Spark.post("/user", this::registerUser);
+        Spark.post("/session", this::login);
+        Spark.delete("/session", this::logout);
+        Spark.get("/game", this::listGames);
+        Spark.post("/game", this::createGame);
+        Spark.delete("/db", this::deleteEverything);
 
         //This line initializes the server and can be removed once you have a functioning endpoint 
         Spark.init();
