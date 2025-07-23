daa7a0cd863eb402c04277f1d7508596b1144d21
edenevans111 <edenevans111@gmail.com>
2025-07-22 20:30:56 -0700

    I am now passing all but two of the tests, turns out there was a stupid thing that I needed to fix in mySQL

--- server/src/main/java/dataaccess/SQLGameDAO.java
+++ server/src/main/java/dataaccess/SQLGameDAO.java
@@ -37,35 +37,36 @@ public class SQLGameDAO implements GameDAO{
     @Override
     public int createGame(String whiteUsername, String blackUsername, String gameName) throws DataAccessException {
         String statement = "INSERT INTO gameData (whiteUsername, blackUsername, gameName, chessGame) VALUES (?, ?, ?, ?)";
-        ChessGame game = new ChessGame();
-        String gameJson = gson.toJson(game);
+        ChessGame chessGame = new ChessGame();
+        String gameJson = gson.toJson(chessGame);
 
         try(var conn = DatabaseManager.getConnection();
                 var ps = conn.prepareStatement(statement,
-                        Statement.RETURN_GENERATED_KEYS)){
+                        new String[]{"gameID"})){
                 ps.setString(1, whiteUsername);
                 ps.setString(2, blackUsername);
                 ps.setString(3, gameName);
                 ps.setString(4, gameJson);
 
-                ps.executeUpdate();
-                try (ResultSet rs = ps.getGeneratedKeys()){
-                    if(rs.next()){
-                        return rs.getInt(1);
-                    } else {
-                        throw new DataAccessException("Did not get generated gameID");
-                    }
+            ps.executeUpdate();
+            try (ResultSet rs = ps.getGeneratedKeys()){
+                if(rs.next()){
+                    return rs.getInt(1);
+                } else {
+                    throw new DataAccessException("Did not get generated gameID");
                 }
-            } catch (SQLException ex) {
-                throw new DataAccessException("Error: unable to create game " + ex.getMessage());
+            }
+        } catch (SQLException ex) {
+            // here is where the creation is having a problem
+            throw new DataAccessException("Error: unable to create game " + ex.getMessage());
         }
     }
 
     @Override
     public GameData getGame(int gameID) throws DataAccessException {
         String statement = "SELECT * FROM gameData WHERE gameID=?";
-        try(var conn = DatabaseManager.getConnection()){
-            var ps = conn.prepareStatement(statement);{
+        try(var conn = DatabaseManager.getConnection();
+            var ps = conn.prepareStatement(statement)){
                 ps.setInt(1, gameID);
                 try (ResultSet rs = ps.executeQuery()){
                     if (rs.next()){
@@ -74,9 +75,8 @@ public class SQLGameDAO implements GameDAO{
                         throw new DataAccessException("Error: bad request");
                     }
                 }
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
+            } catch (SQLException e) {
+            throw new DataAccessException("Error: " + e.getMessage());
         }
     }
 
@@ -84,25 +84,26 @@ public class SQLGameDAO implements GameDAO{
     public Collection<GameData> listGames() throws DataAccessException {
        String statement = "SELECT * FROM gameData";
        Collection<GameData> games = new ArrayList<>();
-       try (var conn = DatabaseManager.getConnection()){
-           var ps = conn.prepareStatement(statement);
-           var rs = ps.executeQuery();
+       try (var conn = DatabaseManager.getConnection();
+            var ps = conn.prepareStatement(statement);
+            var rs = ps.executeQuery()){
            while(rs.next()){
                games.add(readGame(rs));
            }
        } catch (SQLException e) {
-           throw new RuntimeException(e);
+           throw new DataAccessException(e.getMessage());
        }
         return games;
     }
 
     @Override
     public void updateGame(GameData gameData) throws DataAccessException {
-        String statement = "UPDATE gameData SET whiteUsername=?, blackUsername=?, gameName=?, game=? WHERE gameID=?";
+        String statement = "UPDATE gameData SET whiteUsername=?, blackUsername=?, gameName=?, " +
+                "chessGame=? WHERE gameID=?";
 
-        try (var conn = DatabaseManager.getConnection()){
+        try (var conn = DatabaseManager.getConnection();
+             var ps = conn.prepareStatement(statement)){
             String gameJson = gson.toJson(gameData.game());
-            var ps = conn.prepareStatement(statement);
             ps.setString(1, gameData.whiteUsername());
             ps.setString(2, gameData.blackUsername());
             ps.setString(3, gameData.gameName());
@@ -120,7 +121,7 @@ public class SQLGameDAO implements GameDAO{
     private final String[] createStatements = {
             """
            CREATE TABLE IF NOT EXISTS gameData (
-            gameID int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+            gameID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
             whiteUsername VARCHAR(255),
             blackUsername VARCHAR(255),
             gameName VARCHAR(255) NOT NULL,
@@ -168,9 +169,8 @@ public class SQLGameDAO implements GameDAO{
         String whiteUsername = rs.getString("whiteUsername");
         String blackUsername = rs.getString("blackUsername");
         String gameName = rs.getString("gameName");
-        String gameJson = rs.getString("game");
+        String gameJson = rs.getString("chessGame");
         ChessGame gameObj = gson.fromJson(gameJson, ChessGame.class);
         return new GameData(gameID, whiteUsername, blackUsername, gameName, gameObj);
     }
-
 }
