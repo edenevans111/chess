f6203b9c426368d83ff68a31a8954b13e1b26d94
edenevans111 <edenevans111@gmail.com>
2025-07-28 14:48:05 -0700

    I now have the chessboard displaying a bit better, still need to fix some of the spacing

--- client/src/main/java/ui/ChessBoardPrinter.java
+++ client/src/main/java/ui/ChessBoardPrinter.java
@@ -35,7 +35,8 @@ public class ChessBoardPrinter implements BoardDisplay{
                 fewerSpaces + "g" + fewerSpaces + "h" + "\n");
         for (int i = 8; i > 0; i--) {
             StringBuilder row = new StringBuilder();
-            row.append(SET_BG_COLOR_LIGHT_GREY + "%s");
+            row.append(SET_BG_COLOR_LIGHT_GREY + SET_TEXT_COLOR_BLACK + i);
+            System.out.print(row);
             boolean startWhite = i % 2 == 0;
             printRow(startWhite, i, game);
         }
@@ -61,57 +62,46 @@ public class ChessBoardPrinter implements BoardDisplay{
             // if the rowNum == position.getRow && i == position.getColumn()
             // then isHighlighted = true, else: false
             printSquare(isWhite, false, game.getBoard().getPiece(position));
-            System.out.print(fewerSpaces);
+            //System.out.print(fewerSpaces);
         }
         System.out.print("\n");
     }
 
     private void printSquare(boolean isWhite, boolean isHighlighted, ChessPiece piece){
         StringBuilder square = new StringBuilder();
-        if(piece.getPieceType() == null){
-            if(isWhite){
-                square.append(SET_BG_COLOR_LIGHT_GREY);
-            } else {
-                square.append(SET_BG_COLOR_DARK_GREY);
-            }
-        }
-
-
-        ChessPiece.PieceType pieceType = piece.getPieceType();
-        if(pieceType == null){
-            System.out.print("Hi");
-        }
-        ChessGame.TeamColor color = piece.getTeamColor();
 
         if(isHighlighted){
             square.append(SET_BG_COLOR_MAGENTA);
-        }
-        else if(isWhite){
+        } else if (isWhite){
             square.append(SET_BG_COLOR_LIGHT_GREY);
         } else {
-            square.append(SET_TEXT_COLOR_DARK_GREY);
+            square.append(SET_BG_COLOR_BLUE);
         }
 
-        if(color == ChessGame.TeamColor.WHITE){
-            switch(pieceType){
-                case KING -> square.append(SET_TEXT_COLOR_WHITE + WHITE_KING);
-                case QUEEN -> square.append(SET_TEXT_COLOR_WHITE + WHITE_QUEEN);
-                case BISHOP -> square.append(SET_TEXT_COLOR_WHITE + WHITE_BISHOP);
-                case ROOK -> square.append(SET_TEXT_COLOR_WHITE + WHITE_ROOK);
-                case KNIGHT -> square.append(SET_TEXT_COLOR_WHITE + WHITE_KNIGHT);
-                case PAWN -> square.append(SET_TEXT_COLOR_WHITE + WHITE_PAWN);
-                case null -> square.append("  ");
-            }
+        if(piece == null){
+            square.append("   ");
         } else {
-            switch (pieceType) {
-                case KING -> square.append(SET_TEXT_COLOR_BLACK + BLACK_KING);
-                case QUEEN -> square.append(SET_TEXT_COLOR_BLACK + BLACK_QUEEN);
-                case BISHOP -> square.append(SET_TEXT_COLOR_BLACK + BLACK_BISHOP);
-                case ROOK -> square.append(SET_TEXT_COLOR_BLACK + BLACK_ROOK);
-                case KNIGHT -> square.append(SET_TEXT_COLOR_BLACK + BLACK_KNIGHT);
-                case PAWN -> square.append(SET_TEXT_COLOR_BLACK + BLACK_PAWN);
-                // I will probably need to adjust this spacing to be the proper size later
-                case null -> square.append("   ");
+            ChessPiece.PieceType pieceType = piece.getPieceType();
+            ChessGame.TeamColor color = piece.getTeamColor();
+
+            if(color == ChessGame.TeamColor.WHITE){
+                switch(pieceType){
+                    case KING -> square.append(SET_TEXT_COLOR_WHITE + WHITE_KING);
+                    case QUEEN -> square.append(SET_TEXT_COLOR_WHITE + WHITE_QUEEN);
+                    case BISHOP -> square.append(SET_TEXT_COLOR_WHITE + WHITE_BISHOP);
+                    case ROOK -> square.append(SET_TEXT_COLOR_WHITE + WHITE_ROOK);
+                    case KNIGHT -> square.append(SET_TEXT_COLOR_WHITE + WHITE_KNIGHT);
+                    case PAWN -> square.append(SET_TEXT_COLOR_WHITE + WHITE_PAWN);
+                }
+            } else {
+                switch (pieceType) {
+                    case KING -> square.append(SET_TEXT_COLOR_BLACK + BLACK_KING);
+                    case QUEEN -> square.append(SET_TEXT_COLOR_BLACK + BLACK_QUEEN);
+                    case BISHOP -> square.append(SET_TEXT_COLOR_BLACK + BLACK_BISHOP);
+                    case ROOK -> square.append(SET_TEXT_COLOR_BLACK + BLACK_ROOK);
+                    case KNIGHT -> square.append(SET_TEXT_COLOR_BLACK + BLACK_KNIGHT);
+                    case PAWN -> square.append(SET_TEXT_COLOR_BLACK + BLACK_PAWN);
+                }
             }
         }
         System.out.print(square);
