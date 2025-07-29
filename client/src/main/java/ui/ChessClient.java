f59db6708dedf74729b7605603b32482df92c302
edenevans111 <edenevans111@gmail.com>
2025-07-29 10:16:35 -0700

    I have now added functionality for the register method in ChessClient

--- client/src/main/java/ui/ChessClient.java
+++ client/src/main/java/ui/ChessClient.java
@@ -1,7 +1,9 @@
 package ui;
 
 import dataaccess.DataAccessException;
+import request.RegisterRequest;
 import response.LoginResponse;
+import response.RegisterResponse;
 import server.*;
 import request.LoginRequest;
 
@@ -20,8 +22,8 @@ public class ChessClient {
     }
 
     public void preloginHelp(){
-        System.out.print("Register: supply username, password, email\n");
-        System.out.print("Login: username and password\n");
+        System.out.print("Register: supply username, password, email to create an account\n");
+        System.out.print("Login: username and password to login to account\n");
         System.out.print("Quit: exit the program\n");
     }
 
@@ -33,14 +35,35 @@ public class ChessClient {
             loginString.append("No password was given");
         } else if (username.isBlank()){
             loginString.append("No username was given");
+        } else {
+            LoginRequest request = new LoginRequest(username, password);
+            LoginResponse response = serverFacade.login(request);
+            loginString.append("Congratulations, you are logged in as " + response.username());
         }
-        LoginRequest request = new LoginRequest(username, password);
-        LoginResponse response = serverFacade.login(request);
-        loginString.append("Congratulations, you are logged in as " + response.username());
         return loginString.toString();
     }
 
-    //
+    public String register(String [] args) throws DataAccessException {
+        String username = args[1];
+        String password = args[2];
+        String email = args[3];
+        StringBuilder registerString = new StringBuilder();
+
+        if(username.isBlank()){
+            registerString.append("No username given");
+        } else if (password.isBlank()){
+            registerString.append("No password was given");
+        } else if (email.isBlank()){
+            registerString.append("No email was given");
+        } else {
+            // I might want to change this to a try and catch thing later on, so that I can better handle the errors
+            RegisterRequest request = new RegisterRequest(username, password, email);
+            RegisterResponse response = serverFacade.register(request);
+            registerString.append("Welcome! You are now signed in as " + response.username());
+        }
+        return registerString.toString();
+    }
+    
 
 
 
